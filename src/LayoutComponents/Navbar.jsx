759137d485316f80316f0c3762f558184441fa79
farabi islam <farabii099@gmail.com>
2025-04-23 10:56:47 +0600

    navbar changed

--- src/LayoutComponents/Navbar.jsx
+++ src/LayoutComponents/Navbar.jsx
@@ -3,12 +3,33 @@ import { Link } from 'react-router'
 
 const Navbar = () => {
   return (
-    <div className='flex gap-6 text-white bg-black'>
-      <Link to="/">Home</Link>
-      <Link to="/contact">Contact</Link>
-      <Link to="/about">About</Link>
+    <div>
+      <div className="flex justify-center  text-white bg-black px-4 py-1">
+        <small className="flex-1 text-center">
+          Summer sale for All Swim Suits And Free Express Delivery - OFF 50%!
+        </small>
+        <select className="text-white bg-black " name="translate" id="">
+          <option value="English">English</option>
+          <option value="Bangla">Bangla</option>
+        </select>
+      </div>
+      <div className="flex justify-around">
+        <div className='text-2xl font-bold '> Exclusive </div>
+        <div className="flex gap-6">
+          <Link to="/">Home</Link>
+          <Link to="/contact">Contact</Link>
+          <Link to="/about">About</Link>
+        </div>
+        <div>
+          <input
+            type="text"
+            className="outline-none"
+            placeholder="what are you looking for?"
+          />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Navbar
