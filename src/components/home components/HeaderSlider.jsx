e501d1e49d03eb57e00077d334d06d90820e68ad
farabi islam <farabii099@gmail.com>
2025-04-28 13:18:46 +0600

    header slider done

---
+++ src/components/home components/HeaderSlider.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router";
+import { FaArrowRight } from "react-icons/fa6";
+import iphone from "../../assets/apple_logo.png";
+import banner from "../../assets/banner.png";
+const HeaderSlider = ({title,desc}) => {
+  return (
+    <div className="flex bg-black text-white">
+      <div className=" flex flex-col gap-3 w-1/2 p-8 justify-center ">
+        <div className="flex items-center gap-2">
+          <img src={iphone} alt="img" />
+          <p>{title}</p>
+        </div>
+        <h1 className="text-6xl font-bold   ">{desc}</h1>
+        <Link className="flex items-center gap-3 underline">
+          Shop Now <FaArrowRight />
+        </Link>
+      </div>
+      <div className="w-1/2">
+        <img src={banner} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default HeaderSlider;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,45 +1,82 @@
-import React from 'react';
-import HomeSectionHeading from '../components/home components/HomeSectionHeading';
+import React from "react";
+import HomeSectionHeading from "../components/home components/HomeSectionHeading";
+import { Swiper, SwiperSlide} from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination, Autoplay, Navigation } from "swiper/modules";
+import HeaderSlider from "../components/home components/HeaderSlider";
 
 const Home = () => {
-    return <div className="w-11/12 mx-auto px-4 ">
-        {/* header */}
-        <div>
-            <div>
+  return (
+    <div className="w-11/12 mx-auto px-4 ">
+      {/* header */}
+      <div className="flex ">
+        <div className="w-1/3">sidebar</div>
 
-            </div>
-            
+        <div className="w-2/3">
+          <Swiper
+            autoplay={{
+              delay: 2500,
+              disableOnInteraction: false,
+            }}
+            pagination={{
+              clickable: true,
+            }}
+            navigation={true}
+            modules={[Pagination, Autoplay]}
+            className="mySwiper  "
+            style={{
+              "--swiper-pagination-color": "red",
+              "--swiper-pagination-bullet-inactive-color": "#999999",
+              "--swiper-pagination-bullet-inactive-opacity": ".8",
+              "--swiper-pagination-bullet-size": "12px",
+              "--swiper-pagination-bullet-horizontal-gap": "6px",
+            }}
+          >
+            <SwiperSlide>
+              <HeaderSlider
+                title={"iphone 14 series"}
+                desc={"Up to 10%  off Voucher"}
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <HeaderSlider
+                title={"iphone 14 series"}
+                desc={"Up to 10%  off Voucher"}
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <HeaderSlider
+                title={"iphone 14 series"}
+                desc={"Up to 10%  off Voucher"}
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <HeaderSlider
+                title={"iphone 14 series"}
+                desc={"Up to 10%  off Voucher"}
+              />
+            </SwiperSlide>
+          </Swiper>
         </div>
-        <HomeSectionHeading title={"Today’s"} name={"Flash Sales"} />
-        {/* flash sales */}
-        <div>
-
-        </div>
-        {/* categories */}
-        <div>
-
-        </div>
-        {/* best selling prodeucts */}
-         <div>
-
-         </div>
-         {/* banner */}
-         <div>
-
-         </div>
-         {/* our products */}
-         <div>
-
-         </div>
-         {/* new arrival */}
-          <div>
-
-          </div>
-          {/* services */}
-          <div>
-
-          </div>
-    </div>;
+      </div>
+      <HomeSectionHeading title={"Today’s"} name={"Flash Sales"} />
+      {/* flash sales */}
+      <div></div>
+      {/* categories */}
+      <div></div>
+      {/* best selling prodeucts */}
+      <div></div>
+      {/* banner */}
+      <div></div>
+      {/* our products */}
+      <div></div>
+      {/* new arrival */}
+      <div></div>
+      {/* services */}
+      <div></div>
+    </div>
+  );
 };
 
 export default Home;
