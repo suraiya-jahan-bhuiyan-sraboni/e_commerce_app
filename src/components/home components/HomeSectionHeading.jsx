294fabf3aeca1a7e26ed7a6d0ffecf7c491c640e
suraiya-jahan-bhuiyan-sraboni <suraiya.jaha.bhuiyan.12@gmail.com>
2025-04-28 11:12:13 +0600

    home section component done

---
+++ src/components/home components/HomeSectionHeading.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HomeSectionHeading = ({title, name}) => {
+  console.log(title, name)
+  return (
+    <div>
+      <p className='text-red-600 font-semibold '><span className='border-10 rounded-lg mr-4'></span>{title}</p>
+      <h1 className='text-3xl font-bold py-3'>{ name}</h1>
+    </div>
+  )
+}
+
+export default HomeSectionHeading
