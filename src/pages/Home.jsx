13600a5d144cf707ecb1bad4145269fe9ebf978d
farabi islam <farabii099@gmail.com>
2025-04-30 22:29:20 +0600

    sidebar dropdown

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,43 +1,104 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import HomeSectionHeading from "../components/home components/HomeSectionHeading";
 import HeaderSliderComponent from "../components/home components/HeaderSliderComponent";
 import { Link, NavLink } from "react-router";
 import { MdKeyboardArrowRight } from "react-icons/md";
 import Service from "../components/home components/Service";
 const Home = () => {
+  //men's dropdown
+  const [isActive1, setIsActive1] = useState(false);
+  const dropdownRef1 = useRef(null);
+  //women's dropdown
+  const [isActive2, setIsActive2] = useState(false);
+  const dropdownRef2 = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        dropdownRef1.current &&
+        !dropdownRef1.current.contains(event.target)
+      ) {
+        setIsActive1(false);
+      }
+
+      if (
+        dropdownRef2.current &&
+        !dropdownRef2.current.contains(event.target)
+      ) {
+        setIsActive2(false);
+      }
+      console.log("click");
+      
+    };
+
+    document.addEventListener("pointerdown", handleClickOutside);
+    return () =>
+      document.removeEventListener("pointerdown", handleClickOutside);
+  }, []);
+
   return (
     <div className="w-11/12 mx-auto px-4 ">
       {/* header */}
       <div className="flex">
         <div className=" flex flex-col w-1/4 text-[1.2rem] gap-1 border-r border-gray-200 pt-8 pr-4 ">
-          <div className="dropdown dropdown-right ">
-            <NavLink className=" flex items-center justify-between ">
-              Woman’s Fashion <MdKeyboardArrowRight />
-            </NavLink>
-            <ul
-              className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+          <div ref={dropdownRef1} className="dropdown dropdown-right">
+            <Link
+              onClick={() => setIsActive1(!isActive1)}
+              className={`flex items-center justify-between ${
+                isActive1 ? "text-red-500" : ""
+              }`}
             >
-              <li>
-                <Link>Jeans</Link>
-              </li>
-              <li>
-                <Link>T-shirt</Link>
-              </li>
-              <li>
-                <Link>Shirt</Link>
-              </li>
-              <li>
-                <Link>Panjabi</Link>
-              </li>
-              <li>
-                <Link>Shoes</Link>
-              </li>
-            </ul>
+              Men’s Fashion <MdKeyboardArrowRight />
+            </Link>
+
+            {isActive1 && (
+              <ul className="dropdown-content text-[1.2rem] menu bg-base-100 rounded-box z-10 w-52 p-2 shadow-sm ml-1">
+                <li>
+                  <Link>Jeans</Link>
+                </li>
+                <li>
+                  <Link>T-shirt</Link>
+                </li>
+                <li>
+                  <Link>Shirt</Link>
+                </li>
+                <li>
+                  <Link>Panjabi</Link>
+                </li>
+                <li>
+                  <Link>Shoes</Link>
+                </li>
+              </ul>
+            )}
           </div>
 
-          <div className="flex items-center justify-between ">
-            <Link>Men’s Fashion</Link>
-            <MdKeyboardArrowRight />
+          <div ref={dropdownRef2} className="dropdown dropdown-right">
+            <Link
+              onClick={() => setIsActive2(!isActive2)}
+              className={`flex items-center justify-between ${
+                isActive2 ? "text-red-500" : ""
+              }`}
+            >
+              Woman's Fashion
+              <MdKeyboardArrowRight />
+            </Link>
+
+            {isActive2 && (
+              <ul className="dropdown-content text-[1.2rem] menu bg-base-100 rounded-box z-10 w-52 p-2 shadow-sm">
+                <li>
+                  <Link>Jeans</Link>
+                </li>
+                <li>
+                  <Link>T-shirt</Link>
+                </li>
+                <li>
+                  <Link>Shirt</Link>
+                </li>
+                <li>
+                  <Link>Shoes</Link>
+                </li>
+              </ul>
+            )}
           </div>
           <Link>Electronics</Link>
           <Link>Home & Lifestyle</Link>
