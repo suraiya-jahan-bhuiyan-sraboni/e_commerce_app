e5295ac1a51f781f07ba23a6cf167d74d7ec4ddf
suraiya-jahan-bhuiyan-sraboni <suraiya.jaha.bhuiyan.12@gmail.com>
2025-05-01 11:45:03 +0600

    small fix to error page

--- src/pages/_404Error.jsx
+++ src/pages/_404Error.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Breadcrumbs from '../components/Breadcrumbs'
 import Navbar from '../LayoutComponents/Navbar'
 import Footer from '../LayoutComponents/Footer'
+import { Link } from 'react-router'
 
 const _404Error = () => {
   return (
@@ -12,8 +13,9 @@ const _404Error = () => {
         <div className='text-center  py-30 flex flex-col gap-4 items-center'>
           <h1 className='text-7xl'>404 Not Found</h1>
           <p className=''>Your visited page not found. You may go home page.</p>
+          <Link to="/">
           <button type="button" className=" px-8 py-3 font-semibold rounded-md bg-red-400 dark:text-gray-50">Back to home page</button>
-
+          </Link>
         </div>
 
       </div>
